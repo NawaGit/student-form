fbb64533887af9837a77664260000b973857c973
MSamiDev <mohammadsami@duck.com>
2023-02-16 23:58:08 +0530

    add: delete and update button, delete api

--- src/components/studentTableView.jsx
+++ src/components/studentTableView.jsx
@@ -1,4 +1,19 @@
-const StudentsTableView = ({ students, search}) => {
+import axios from "axios";
+
+const StudentsTableView = ({ students, search }) => {
+	// write a funtion to delete specific students
+	const deleteStudent = async (id) => {
+		const url = `http://localhost:3000/students/${id}`;
+		axios
+			.delete(url)
+			.then(function (response) {
+				console.log(JSON.stringify(response.data));
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	};
+		
 	return (
 		<>
 			<div className="px-48 sm:px-16">
@@ -27,44 +42,82 @@ const StudentsTableView = ({ students, search}) => {
 								<th scope="col" className="py-3 px-6">
 									Address
 								</th>
+								<th scope="col" className="py-3 px-6"></th>
+								<th scope="col" className="py-3 px-6"></th>
 							</tr>
 						</thead>
 						<tbody>
-							{students.filter((val)=>{
-								if (search == ""){
-									return val
-								}
-								else if (val.firstName.toLowerCase().includes(search.toLowerCase())){
-									return val
-								}
-								else if (val.lastName.toLowerCase().includes(search.toLowerCase())){
-									return val
-								}
-								else if (val.college.toLowerCase().includes(search.toLowerCase())){
-									return val
-								}
-								else if (val.department.toLowerCase().includes(search.toLowerCase())){
-									return val
-								}
-							}).map((student) => (
-								<tr key={students.id} className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-									<th
-										scope="row"
-										className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+							{students
+								.filter((val) => {
+									if (search === "") {
+										return val;
+									} else if (
+										val.firstName
+											.toLowerCase()
+											.includes(search.toLowerCase())
+									) {
+										return val;
+									} else if (
+										val.lastName
+											.toLowerCase()
+											.includes(search.toLowerCase())
+									) {
+										return val;
+									} else if (
+										val.college
+											.toLowerCase()
+											.includes(search.toLowerCase())
+									) {
+										return val;
+									} else if (
+										val.department
+											.toLowerCase()
+											.includes(search.toLowerCase())
+									) {
+										return val;
+									}
+									return undefined;
+								})
+								.map((student, index) => (
+									<tr
+										key={students.id}
+										className="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
 									>
-										{student.firstName} {student.lastName}
-									</th>
-									<td className="py-4 px-6">{student.email}</td>
-									<td className="py-4 px-6"> {student.phone} </td>
-									<td className="py-4 px-6"> {student.age} </td>
-									<td className="py-4 px-6"> {student.college} </td>
-									<td className="py-4 px-6"> {student.department} </td>
-									<td className="py-4 px-6">
-										{student.address}, {student.city}, {student.state}
-										, {student.country}, {student.zipCode}
-									</td>
-								</tr>
-							))}
+										<th
+											scope="row"
+											className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+										>
+											{student.firstName} {student.lastName}
+										</th>
+										<td className="py-4 px-6">{student.email}</td>
+										<td className="py-4 px-6"> {student.phone} </td>
+										<td className="py-4 px-6"> {student.age} </td>
+										<td className="py-4 px-6"> {student.college} </td>
+										<td className="py-4 px-6">
+											{student.department}
+										</td>
+										<td className="py-4 px-6">
+											{student.address}, {student.city},
+											{student.state}, {student.country},
+											{student.zipCode}
+										</td>
+										<td className="py-4 px-6">
+											<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+												Update
+											</button>
+										</td>
+										<td className="py-4 px-6">
+											
+											<button
+											onClick={() => deleteStudent(student._id)}
+												className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+											>
+											Delete
+											</button>
+
+										</td>
+									</tr>
+								))}
 
 							{/* <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
 								<th
